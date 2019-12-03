7676b055b725c46be5d6d7264ca832dfde77d62f
Nouman Arshad <noumanarshad0320@gmail.com>
2019-12-03 23:18:11 +0500

    Initialized a mean stack project

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const mongoose = require('mongoose');
+const routes = require('./server/routes');
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'dist/eCommerce-mean-app')));
+
+app.use('/api', routes);
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/eCommerce-mean-app/index.html'));
+});
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce-mean-app');
+mongoose.connection.on('error', console.error.bind(console, 'Database connection error:'));
+
+mongoose.connection.once('open', function () {
+  console.info('Successfully connected to the database');
+});
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, function () {
+  console.info(`Server started on http://localhost:${port}`)
+});
